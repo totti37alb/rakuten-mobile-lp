f2a1d447cc7d8334a1b21ac5f441c8c47b42be10
totti37alb <yuuya08037@gmail.com>
2026-06-07 10:55:52 +0900

    remove: delete low-quality articles

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,8 @@
 @echo off
 cd /d "%~dp0"
+del "content\articles\rakuten-point-guide-2026.md"
+del "content\articles\2026-06-07-rakuten-mobile-point-maximize.md"
 git add -A
-git commit -m "style: article design improvements - H2 red border, callout boxes, red bullets, decorative HR"
+git commit -m "remove: delete low-quality articles"
 git push origin main
-pause
+git log --oneline -3 > git-last-result.txt
