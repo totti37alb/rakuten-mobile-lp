6555adb88c5707bb71770946bbdc81e95c70551a
totti <yuuya08037@gmail.com>
2026-06-09 19:49:44 +0900

    content: rewrite all articles with conversational writing style

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 git add -A
-git commit -m "fix: add remark-gfm for markdown table support, rewrite coverage article"
+git commit -m "content: rewrite all articles with conversational writing style"
 git push origin main
 git log --oneline -3 > git-last-result.txt
