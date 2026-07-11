413366c2acfb7fb012dc1fd54d2700d9ad08aef6
totti <yuuya08037@gmail.com>
2026-07-11 10:08:04 +0900

    add: keiba kiji ni gazou tsuika

--- git-push.bat
+++ git-push.bat
@@ -2,6 +2,6 @@
 cd /d "%~dp0"
 if exist .git\index.lock del /f .git\index.lock
 git add -A
-git commit -m "add: kiji tsuika 2026-07-11 keiba-carryover-ai"
+git commit -m "add: keiba kiji ni gazou tsuika"
 git push origin main
 git log --oneline -3 > git-last-result.txt
