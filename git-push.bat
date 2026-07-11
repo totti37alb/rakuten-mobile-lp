ecc963c71060f06846d7f167040fdee6ddd0078e
totti <yuuya08037@gmail.com>
2026-07-11 09:24:21 +0900

    auto: kiji tsuika 2026-07-11 saikyo-kanshasai

--- git-push.bat
+++ git-push.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
+if exist .git\index.lock del /f .git\index.lock
 git add -A
-git commit -m "design: overhaul PersonalIntro, Benefits, Articles, FAQ, CTA sections"
+git commit -m "auto: kiji tsuika 2026-07-11 saikyo-kanshasai"
 git push origin main
 git log --oneline -3 > git-last-result.txt
