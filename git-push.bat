ab2e137d73a404f4ecc0a4eecdd46d142587a339
totti <yuuya08037@gmail.com>
2026-08-15 08:39:03 +0900

    auto: kiji tsuika 2026-08-15

--- git-push.bat
+++ git-push.bat
@@ -1,7 +1,8 @@
 @echo off
 cd /d "%~dp0"
 if exist .git\index.lock del /f .git\index.lock
+if exist .git\HEAD.lock del /f .git\HEAD.lock
 git add -A
-git commit -m "add: keiba kiji ni gazou tsuika"
+git commit -m "auto: kiji tsuika 2026-08-15"
 git push origin main
 git log --oneline -3 > git-last-result.txt
